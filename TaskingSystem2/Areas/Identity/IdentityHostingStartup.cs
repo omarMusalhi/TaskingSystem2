2bf42c303b511c5d948e5ff0b36738aba19085c2
Omar Al Musalhi <almbher999@hotmail.com>
2020-12-22 14:03:33 +0400

    Add project files.

---
+++ TaskingSystem2/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TaskingSystem2.Data;
+
+[assembly: HostingStartup(typeof(TaskingSystem2.Areas.Identity.IdentityHostingStartup))]
+namespace TaskingSystem2.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ TaskingSystem2/Models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskingSystem2.Models
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string DepartmentName { get; set; }
+
+        [Required]
+        public string SectionName { get; set; }
+    }
+}
